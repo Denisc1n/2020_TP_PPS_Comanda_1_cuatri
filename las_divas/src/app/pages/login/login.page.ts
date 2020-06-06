8bc8f797fd43ecbf698bf0eb5be9075aef8d7446
pmatwiji <pmatwiji@gmail.com>
2020-06-05 21:11:23 -0300

    branch

--- las_divas/src/app/pages/login/login.page.ts
+++ las_divas/src/app/pages/login/login.page.ts
@@ -16,6 +16,7 @@ export class LoginPage implements OnInit {
   showBackdrop = true;
   mostrarFrmLogin : boolean = false;
   mostrarFrmInvitado : boolean = false;
+  
   listado : any = [];
   email : string;
   pass : string;
