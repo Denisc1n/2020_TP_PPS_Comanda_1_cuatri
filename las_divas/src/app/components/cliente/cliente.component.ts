4212b5c83838905dd690ff2e1470ddd556a865a4
rosasthomas <rosasthomas@gmail.com>
2020-06-13 19:44:01 -0300

    backup

--- las_divas/src/app/components/cliente/cliente.component.ts
+++ las_divas/src/app/components/cliente/cliente.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { QRScannerService } from 'src/app/servicios/qrscanner.service';
+import { FirebaseService } from 'src/app/servicios/firebase.service';
 
 @Component({
   selector: 'app-cliente',
@@ -7,8 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteComponent implements OnInit {
 
-  constructor() {}
+  currentUser
+  dataCurrentUser
+
+  constructor(private QRService:QRScannerService, private fireService:FirebaseService) {
+    this.currentUser = fireService.getCurrentUser()
+    fireService .getDBByDoc('cliente', this.currentUser.email).then(data=>this.dataCurrentUser=data)
+  }
 
   ngOnInit() {}
 
+  scanListaDeEspera(){
+    this.QRService.scan().then((a:any)=>{
+      if(a.text == 'listaDeEsperaLasDivas'){
+        this.fireService.createDocInDB('listaEspera', this.currentUser.email, this.dataCurrentUser)
+      }
+      else{
+        console.error('Primero debe ir a la lista de espera')
+      }
+    })
+  }
 }
