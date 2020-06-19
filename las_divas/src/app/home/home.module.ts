90ea47c39fb68b7ad049203d61cad515586da335
pmatwiji <pmatwiji@gmail.com>
2020-06-19 18:41:42 -0300

    notificaciones mesas

--- las_divas/src/app/home/home.module.ts
+++ las_divas/src/app/home/home.module.ts
@@ -7,12 +7,13 @@ import { DuenioComponent } from '../components/duenio/duenio.component';
 import { HomeMozoComponent } from '../components/mozo/home-mozo/home-mozo.component';
 import { PagosComponent } from '../components/mozo/pagos/pagos.component';
 import { PedidosComponent } from '../components/mozo/pedidos/pedidos.component';
+import { ConsultasComponent } from '../components/mozo/consultas/consultas.component';
 import { ClienteComponent } from '../components/cliente/cliente.component';
 import { SolicitudesComponent } from '../components/solicitudes/solicitudes.component';
 import { ListadoMesasComponent } from '../components/metre/listado-mesas/listado-mesas.component';
 import { HomeMetreComponent  } from '../components/metre/home-metre/home-metre.component'
 import { HomePageRoutingModule } from './home-routing.module';
-import {ListaEsperaComponent  } from '../components/metre/lista-espera/lista-espera.component'
+import { ListaEsperaComponent  } from '../components/metre/lista-espera/lista-espera.component'
 
 
 @NgModule({
@@ -31,6 +32,7 @@ import {ListaEsperaComponent  } from '../components/metre/lista-espera/lista-esp
     HomeMetreComponent,
     PagosComponent,
     PedidosComponent,
+    ConsultasComponent,
     ListaEsperaComponent,
     ListadoMesasComponent]
 })
