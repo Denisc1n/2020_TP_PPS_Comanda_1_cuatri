684d90b9200eb06dbaec0a81f466cfe99654220d
pmatwiji <pmatwiji@gmail.com>
2020-06-27 01:59:41 -0300

    fotos para readme

--- las_divas/src/app/pipes/bebidas.pipe.ts
+++ las_divas/src/app/pipes/bebidas.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class BebidasPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
+  transform(value: any, ...args: unknown[]): unknown {
 
     let aux = [];
 
